67e3bae0d4ab9e8ad84517a46bab60df079c7608
Yulia Alexeev <july90a@gmail.com>
2021-08-01 20:23:40 +0300

    final upload

--- js/site.js
+++ js/site.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function directTo(currentPage){
+ 
+  console.log(`currentPage`, currentPage)
+  if(currentPage === 'Passengers'){
+    console.log(`true`, true)
+    window.location.href = "/sms.html"; return false;
+  }
+  
+  
+}
